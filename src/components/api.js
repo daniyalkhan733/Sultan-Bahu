5d841498b917b424c6e9a3025756afb1a6a56380
daniyal-p <daniyal733khan@gmail.com>
2025-01-03 16:22:59 +0530

    fix

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
 // api.js
 export const fetchPrograms = async () => {
-    const icharmsUrl = import.meta.env.ICHARMS_URL;
+    const icharmsUrl = "https://6641e98f3d66a67b34356a00.mockapi.io/api/v1/programs-so";
     // const apiKey = import.meta.env.ICHARMS_API_KEY;
   
     const response = await fetch(`${icharmsUrl}`, {
