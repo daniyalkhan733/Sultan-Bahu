0dbdcbe666068072c338dc32200e7a9f310e9edc
daniyal-p <daniyal733khan@gmail.com>
2025-01-10 12:43:29 +0530

    minor changes done

--- src/components/api.js
+++ src/components/api.js
@@ -1,14 +1,14 @@
 // api.js
 export const fetchPrograms = async () => {
-    const icharmsUrl = "https://6641e98f3d66a67b34356a00.mockapi.io/api/v1/programs-so";
-    // const apiKey = import.meta.env.ICHARMS_API_KEY;
-  
-    const response = await fetch(`${icharmsUrl}`, {
-    //   headers: {
-    //     Authorization: `Bearer ${apiKey}`,
-    //   },
-    });
-  
-    const data = await response.json();
-    return data; 
-  };
+  // const icharmsUrl = import.meta.env.ICHARMS_URL;
+  const apiKey = import.meta.env.ICHARMS_API_KEY;
+
+  const response = await fetch("https://sultanbahucenter-icharms.tscube.co.in/api/v1/program-list?limit=1000", {
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+    },
+  });
+console.log(response)
+  const data = await response.json();
+  return data; // Assuming the response is an array of programs
+};
