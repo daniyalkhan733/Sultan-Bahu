3d93ded36f57233b5ff921a8ec1cfc08c1b97ff8
daniyal-p <daniyal733khan@gmail.com>
2025-02-21 19:33:16 +0530

    changes

--- src/components/api.js
+++ src/components/api.js
@@ -1,13 +1,13 @@
 // api.js
 export const fetchPrograms = async () => {
-  // const icharmsUrl = import.meta.env.ICHARMS_URL;
+  const icharmsUrl = import.meta.env.API_URL;
   const apiKey = import.meta.env.ICHARMS_API_KEY;
 
-  const response = await fetch("https://sultanbahucenter-icharms.tscube.co.in/api/v1/program-list?limit=1000", {
+  const response = await fetch(`${icharmsUrl}program-list?limit=1000`, {
     headers: {
       Authorization: `Bearer ${apiKey}`,
     },
   });
   const data = await response.json();
-  return data; // Assuming the response is an array of programs
+  return data; 
 };
